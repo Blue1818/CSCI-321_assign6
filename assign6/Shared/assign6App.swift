6542384df7935b148e2cdf40e2616d9e640f4446
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2022-05-04 14:12:31 -0400

    ceate project

---
+++ assign6/Shared/assign6App.swift
@@ -0,0 +1,17 @@
+//
+//  assign6App.swift
+//  Shared
+//
+//  Created by user214621 on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct assign6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
