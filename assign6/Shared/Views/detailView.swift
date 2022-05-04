17e3fd0e6c533c541015fd91f7085a654cf44f24
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2022-05-04 17:11:50 -0400

    file struct

---
+++ assign6/Shared/Views/detailView.swift
@@ -0,0 +1,8 @@
+//
+//  detailView.swift
+//  assign6 (iOS)
+//
+//  Created by user214621 on 5/4/22.
+//
+
+import Foundation
---
+++ assign6/Shared/Views/presidentCell.swift
@@ -0,0 +1,8 @@
+//
+//  presidentCell.swift
+//  assign6 (iOS)
+//
+//  Created by user214621 on 5/4/22.
+//
+
+import Foundation
